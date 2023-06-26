3dc345fa3fd80f05462bba66fb1a88df84250a0c
olgamartinchik <sintcerova@mail.ru>
2023-06-26 17:46:14 +0300

    feat: add timer task

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './assets/icons/logo.svg';
+// import {SApp, SHeader, SLink, SLogo} from "./assets/styles/app.styles";
+import Timer from './components/timer/Timer';
+
+function App() {
+    return (
+    
+    <Timer/>
+    
+    );
+}
+
+export default App;
