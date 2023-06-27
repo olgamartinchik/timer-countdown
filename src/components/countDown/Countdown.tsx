c55bc95f886e1958528b9d034a67ca1873e2364d
olgamartinchik <sintcerova@mail.ru>
2023-06-27 19:56:04 +0300

    feat: add CountDown task

--- src/components/countDown/Countdown.tsx
+++ src/components/countDown/Countdown.tsx
@@ -1,16 +1,101 @@
-import React, {} from 'react';
-import { Title, StyledCounter } from '../../assets/styles/app.styles';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import { Title, StyledCounter, ContainerItems, TimerStyled } from '../../assets/styles/app.styles';
+import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { Box, LinearProgress, Typography } from '@mui/material';
 
 import TimeInput from './TimeInput';
+// import ProgressTime from './ProgressTime';
+// import ButtonControl from './ButtonControl';
+import { Button,  } from '@mui/material';
 
 
 const Countdown = () => {
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+   
+    const [time, setTime] = useState(0);
+    const [isPlay, setIsPlay] = useState(false)
+    const timer:React.MutableRefObject<null |number | NodeJS.Timeout> = useRef(null)
+
+    const playAudio = () => {
+        const audio = new Audio('https://zvukitop.com/wp-content/uploads/2021/09/zvuk-effekt-konec-6.mp3')
+        
+        audio.play();
+   }
+   useEffect(() => {
+    if (isPlay) {
+      timer.current = setInterval(() => {
+        if (time > 0) {
+          setTime((time) => time - 1);
+        } else {
+          clearInterval(timer.current!);
+          setTime(0)
+          setMinutes(0)
+          setSeconds(0)
+          setIsPlay(false)
+          playAudio();
+        }
+      }, 1000);
+    } else {
+      clearInterval(timer.current!);
+    }
+  
+    return () => {
+      clearInterval(timer.current!);
+    };
+  }, [time, isPlay]);
+   
+    const toggleTimer = useCallback(() => {         
+        setIsPlay(!isPlay) 
+    },[isPlay])
+
+    const resetTimer = useCallback(() => {
+        clearInterval(timer.current!)
+        setTime(0)
+        setMinutes(0)
+        setSeconds(0)
+        setIsPlay(false)
+        console.log('isPlay',isPlay)
+    }, [])
+
+    
+   
+   
+      //progress
+      const getTimerFormat = useMemo (()=>{
+        return (time: number) => {
+            const minutes = Math.floor(time / 60);
+            const seconds = Math.floor((time % 60) );         
+            return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;          
+        }
+    },[]) 
+    
     return (
+      
     <StyledCounter>
-        <Title>Countdown</Title>
-        <TimeInput/>
+        <Title>Countdown</Title> {time}
+        <TimeInput minutes={minutes} setMinutes={setMinutes} seconds={seconds} setSeconds={setSeconds} setTime = {setTime} time={time} isPlay={isPlay}/>
+        
+        {/* <ProgressTime  value={time} /> */}
+        <Box sx={{ display: 'flex', flexDirection:'column', alignItems: 'center', gap:'30px', width: '50%' }}>
+             <TimerStyled>{getTimerFormat(time)}</TimerStyled>
+            <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress variant="determinate" value={time ? Math.round((((minutes*60+seconds) - time) /(minutes*60+seconds)) * 100) : 0} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${time ? Math.round((((minutes*60+seconds) - time) / (minutes*60+seconds)) * 100) : 0}%`}</Typography>
+            </Box>
+        </Box>
+
+        {/* <ButtonControl/> */}
+        <ContainerItems>
+            <Button variant="contained" color="secondary" onClick={toggleTimer}>{!isPlay?<PlayArrowIcon/>:<PauseIcon/>}</Button>
+            <Button variant="contained" color="secondary" onClick={resetTimer}>Reset</Button>            
+        </ContainerItems>
+        
     </StyledCounter>
     )
 
 }
-export default Countdown
+export default React.memo(Countdown)
--- src/components/countDown/TimeInput.tsx
+++ src/components/countDown/TimeInput.tsx
@@ -1,4 +1,5 @@
-import React, {useState, useCallback} from 'react';
+import React, { useCallback} from 'react';
+// import PropTypes from 'prop-types'
 import { styled } from '@mui/material/styles';
 import { Slider } from '@mui/material';
 import MuiInput from '@mui/material/Input';
@@ -18,45 +19,61 @@ const marks = [
     label: '60 min',
   },
 ];
-const TimeInput = () => {
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
+type PropType={
+  setTime:  (value: React.SetStateAction<number>) => void
+  setMinutes:  (value: React.SetStateAction<number>) => void
+  setSeconds:  (value: React.SetStateAction<number>) => void
+  minutes: number
+  seconds: number
+  time: number
+  isPlay:boolean
+}
+
+
+const TimeInput: React.FunctionComponent<PropType>  = ({setTime,setSeconds,setMinutes,minutes, seconds, time, isPlay}) => {
+    // const [minutes, setMinutes] = useState(0);
+    // const [seconds, setSeconds] = useState(0);
    
     
       const handleSliderChange = useCallback((event: Event, newValue: number | number[]) => {
-        console.log('handleSliderChange',newValue)
-        const minutes = Math.floor(+newValue / 60);
-        const seconds = (+newValue % 60) ;
-        setMinutes(minutes);
-        setSeconds(seconds);
+        // console.log('handleSliderChange',newValue)
+        const minute = Math.floor(+newValue / 60);
+        const second = (+newValue % 60) ;
+        setMinutes(minute);
+        setSeconds(second);
+        setTime(minute*60+second)
       }, [minutes, seconds]);
 
       const handleMinuteChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         let value = parseInt(event.target.value);
-        console.log('handleMinuteChange',value)
+        // console.log('handleMinuteChange',value)
         value = Math.min(value, 720);
         setMinutes(value);
-      }, [minutes]);
+        setTime(seconds+value*60)
+      }, [minutes,seconds]);
 
       const handleSecondChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         let value = parseInt(event.target.value);
-        console.log('handleSecondChange',value)
+        // console.log('handleSecondChange',value)
         value = Math.min(value, 59); 
         setSeconds(value);
-      }, [seconds]);
+        setTime(minutes*60+value)
+      }, [seconds, minutes]);
    
-     
+      
 
     return (
         <div>
-            <p>Start time: {minutes}, {seconds}</p>
+            <p>Start time: {minutes}, {seconds}, {minutes*60+seconds}</p>
             <ContainerItems>
              
                 <Input
                     value={minutes}
                     size="small"
-                    onChange={handleMinuteChange}                          
-                    inputProps={{                 
+                    onChange={handleMinuteChange}   
+                    disabled={time === 0 && isPlay}                       
+                    inputProps={{         
+                    disabled: time !== 0 && isPlay,        
                     step: 1,
                     min: 0,
                     max: 720,
@@ -67,8 +84,10 @@ const TimeInput = () => {
                 <Input
                     value={ seconds }
                     size="small"
-                    onChange={handleSecondChange}                                 
+                    onChange={handleSecondChange} 
+                                                
                     inputProps={{
+                    disabled: time !== 0 && isPlay,  
                     step: 1,
                     min: 0,
                     max: 59,
@@ -77,7 +96,7 @@ const TimeInput = () => {
                 }}
                 /><span>sec</span>
                 <Slider
-                    value={minutes*60+seconds}
+                    value={minutes  * 60 + seconds}
                     valueLabelDisplay="on"
                     onChange={handleSliderChange }
                     step={15}          
@@ -85,6 +104,7 @@ const TimeInput = () => {
                     max={3600}
                     color="secondary"
                     marks={marks}
+                    disabled={time !== 0 && isPlay }
                 />
         
             </ContainerItems>
@@ -92,4 +112,13 @@ const TimeInput = () => {
     )
 
 }
+// TimeInput.propTypes = {  
+//   setTime:  PropTypes.func,
+//   setMinutes: PropTypes.func,
+//   setSeconds:  PropTypes.func,
+//   minutes: PropTypes.number,
+//   seconds: PropTypes.number,
+//   time: PropTypes.number,
+//   isPlay: PropTypes.bool,
+// }
 export default React.memo(TimeInput)
